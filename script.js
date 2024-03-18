9878094ed411ed150b5dedb5567c60655a0d0e46
Vidath-02 <163777374+Vidath-02@users.noreply.github.com>
2024-03-18 18:33:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.querySelector('.list-container');
+function addTask(){
+    if (inputBox.value === ''){
+        alert ("You must Write Something");
+
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li); 
+    }
+}
